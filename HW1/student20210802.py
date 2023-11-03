f545a31515758026a4ecf5f698d53479f19b1bc5
YujeongLeeVV <147732943+YujeongLeeVV@users.noreply.github.com>
2023-11-03 20:05:01 +0900

    Update student20210802.py

--- HW1/student20210802.py
+++ HW1/student20210802.py
@@ -9,10 +9,14 @@ write=wf.active
 write.title='Sheet1'
 
 list1=[]
+list2=[]
 list3=[]
+list4=[]
 list5=[]
+
 dic={}
 dic2={}
+dic3={}
 aa=0
 a0=0 
 bb=0 
@@ -49,7 +53,7 @@ while j<=len(list2):
         list3.append(dic2)
         f+=1
         
-    elif j<len(list2)*0.15:
+    elif j<=len(list2)*0.15:
         dic2={list(list2[i].keys())[0]:'A+'}
         list3.append(dic2)      
         aa+=1
@@ -68,11 +72,7 @@ while j<=len(list2):
         dic2={list(list2[i].keys())[0]:'B0'}
         list3.append(dic2)   
         b0+=1
-        
-    elif j<len(list2)*0.85:
-        dic2={list(list2[i].keys())[0]:'C+'}
-        list3.append(dic2)    
-        cc+=1
+  
         
     elif j<len(list2)*1:
         dic2={list(list2[i].keys())[0]:'C0'}
@@ -81,9 +81,64 @@ while j<=len(list2):
         
     i+=1
     j+=1
-    
-list4=sorted(list3, key=lambda item: list(item.keys())[0])
 
+
+if c0>1:    
+    cc=c0//2
+
+countCC=0   
+
+i=0
+j=1
+
+while j<=len(list2):
+    total=list(list2[i].values())[0]
+    if total<40:
+        dic3={list(list2[i].keys())[0]:'F'}
+        list5.append(dic3)
+        f+=1
+        
+    elif j<=len(list2)*0.15:
+        dic3={list(list2[i].keys())[0]:'A+'}
+        list5.append(dic3)      
+        
+    elif j<len(list2)*0.30:
+        dic3={list(list2[i].keys())[0]:'A0'}
+        list5.append(dic3)
+        
+    elif j<len(list2)*0.50:
+        dic3={list(list2[i].keys())[0]:'B+'}
+        list5.append(dic3)
+        
+    elif j<len(list2)*0.70:
+        dic3={list(list2[i].keys())[0]:'B0'}
+        list5.append(dic3)   
+  
+    elif j<len(list2)*0.85:
+        if countCC<cc:
+            dic3={list(list2[i].keys())[0]:'C+'}
+            list5.append(dic3)   
+            countCC+=1   
+        else:
+            dic3={list(list2[i].keys())[0]:'C0'}
+            list5.append(dic3)
+        
+    elif j<len(list2)*1:
+        dic3={list(list2[i].keys())[0]:'C0'}
+        list5.append(dic3)
+        
+    i+=1
+    j+=1
+
+
+list4=sorted(list5, key=lambda item: list(item.keys())[0])
+
+
+print(list3, "list3")
+print(list4, "list4")
+print(list5, "list5")
+print(dic2,"dic2")
+print(dic3,"dic3")
 
 
 i=0
